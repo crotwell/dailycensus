be0314de6e1bec9ba338944952ecd8cb99cddf64
Philip Crotwell <crotwell@seis.sc.edu>
2020-07-06 13:13:25 -0400

    find fixed status in people

--- validate.py
+++ validate.py
@@ -15,6 +15,17 @@ jsonSummary = createSummary(today)
 
 print(json.dumps(jsonSummary, indent=2))
 print()
+
+for f in fixedStatus:
+    found = False
+    for p in config['people']:
+        if p['name'] == f['name']:
+            found = True
+            break
+    if not found:
+        print(f'Unable to find {f["name"]} from fixedStatus')
+
+
 print("{} summary:  {}, out of {}".format(today, jsonSummary['totals'], len(config['people'])))
 
 print("seems ok...")
