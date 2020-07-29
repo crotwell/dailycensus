ff60836a273b1ff2b69846f7e5bc7b17ace427e8
Philip Crotwell <crotwell@seis.sc.edu>
2020-07-29 16:29:34 -0400

    validate checks email

--- validate.py
+++ validate.py
@@ -16,6 +16,16 @@ jsonSummary = createSummary(today)
 print(json.dumps(jsonSummary, indent=2))
 print()
 
+allIsOk = True
+
+for p in config['people']:
+    try:
+        checkValidEmailAddr(p['email'])
+    except:
+        allIsOk = False
+        print(f'Missing email for {p["name"]}')
+
+
 allFound = True
 for f in fixedStatus:
     found = False
@@ -24,6 +34,7 @@ for f in fixedStatus:
             found = True
             break
     if not found:
+        allIsOk = False
         allFound = False
         print(f'Unable to find {f["name"]} from fixedStatus')
 if allFound:
@@ -31,4 +42,7 @@ if allFound:
 
 print("{} summary:  {}, out of {}".format(today, jsonSummary['totals'], len(config['people'])))
 
-print("seems ok...")
+if allIsOk:
+    print("seems ok...")
+else:
+    print("Looks like there are problems!")
