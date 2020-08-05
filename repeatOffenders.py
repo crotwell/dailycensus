3a502586428c18f6814fc77dea526c92f04194f5
Philip Crotwell <crotwell@seis.sc.edu>
2020-08-05 10:41:30 -0400

    summary repeat offenders

---
+++ repeatOffenders.py
@@ -0,0 +1,33 @@
+from common import *
+import datetime
+import time
+
+import os
+import requests
+from pprint import pprint
+
+
+loadPeople(config)
+today=todayAsStr()
+fixedStatus = loadFixedStatus(config)
+
+oneday = datetime.timedelta(days=1)
+
+now = datetime.datetime.now().replace(microsecond=0)
+theday = now
+missedReport = {}
+for p in config['people']:
+    p['missedReport'] = 0
+for d in range(0,5):
+    dayStr=theday.date().isoformat()
+    summary = createSummary(dayStr)
+    for mrp in summary['notReporting']:
+        for p in config['people']:
+            if p['name'] == mrp['name']:
+                p['missedReport'] += 1
+                break
+    theday=theday - oneday
+
+
+for p in config['people']:
+    print(f"Missed: {p['name']} {p['missedReport']}")
